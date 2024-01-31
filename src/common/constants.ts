a49d0fac817fc1b3069095aad1ab0e55bc6638c5
Paulo Victor Rios <victorios.ba@gmail.com>
2024-01-31 16:52:39 -0300

    chore: add constants file

---
+++ src/common/constants.ts
@@ -0,0 +1,14 @@
+export const JWT = {
+  OPTIONS: {
+    // IN MINUTES
+    EXPIRES_IN_MINUTES: 5,
+  },
+};
+// IN MINUTES
+export const CACHE_TTL_MINUTES = 10;
+export const IS_PUBLIC_KEY = 'isPublic';
+export enum Environment {
+  DEVELOPMENT = 'development',
+  PRODUCTION = 'production',
+  TEST = 'test',
+}
