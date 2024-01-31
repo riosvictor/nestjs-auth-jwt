25049c645d9657a4352f718bcbcee0e07e70c234
Paulo Victor Rios <victorios.ba@gmail.com>
2024-01-31 16:00:35 -0300

    chore: adicionando Docker e CI com Github Actions

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,9 +1,12 @@
 export const JWT = {
-  SECRET: '76F367A652BD433D6E21719E83CA6',
   OPTIONS: {
     EXPIRES_IN_MINUTES: 5,
     REFRESH_EXPIRES_IN_MINUTES: 10,
   },
+};
+export const RATE_LIMIT = {
+  TTL_MINUTES: 5,
+  REQUESTS: 10,
 };
 export const CACHE_TTL_IN_MINUTES = 10;
 export const IS_PUBLIC_KEY = 'isPublic';
